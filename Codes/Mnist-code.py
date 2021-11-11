c42c4eda7db4d0c73ed580511fcd7053bb5b810a
Muhammad Saad <muhdsaad107@gmail.com>
2021-11-11 11:12:18 +0500

    ok

--- Codes/Mnist-code.py
+++ Codes/Mnist-code.py
@@ -1,8 +1,10 @@
 from sklearn.datasets import fetch_openml
+# import pandas as pd
 
 mnist = fetch_openml('mnist_784')
 
-x = mnist.data
-y = mnist.target
+x = mnist.data.shape
+y = mnist.target.shape
 
-print(mnist.shape())
+print("Shape of data", x)
+print("Shape of target", y)
